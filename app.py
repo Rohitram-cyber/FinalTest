3ecc9f37fc2b951afe2b0d6d4805e26f596528e2
Rohitram-cyber <rohit29ram@gmail.com>
2025-07-01 22:07:14 +0530

    app.py

--- app.py
+++ app.py
@@ -84,9 +84,11 @@ def download_excel():
 
 
 # Serve uploaded files (for report view/download)
-@app.route('/uploads/<filename>')
+from flask import send_from_directory
+
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
-    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == "__main__":
