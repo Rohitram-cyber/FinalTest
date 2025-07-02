7c352dcef0313a59c735da481c9e370069319de9
Rohitram-cyber <rohit29ram@gmail.com>
2025-07-02 12:43:41 +0530

    app.py

--- app.py
+++ app.py
@@ -154,6 +154,10 @@ def download_file(report_id):
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route("/download-db")
+def download_db():
+    return send_file("reports.db", as_attachment=True)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
